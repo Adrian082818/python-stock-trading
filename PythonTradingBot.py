641da19ad4f7d9e095e8e4ca2349817c72bdf879
Adrian082818 <mohabiradrian@gmail.com>
2020-06-08 09:39:27 -0400

    will add live data streaming for market updates

--- PythonTradingBot.py
+++ PythonTradingBot.py
@@ -1,9 +1,22 @@
 import alpaca_trade_api as tradeapi 
 from alpaca_trade_api import StreamConn
+import threading
+import time
+import datetime
+import logging
+import argparse
+
+#initialize logging to see debug output
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True 
 
 ALPACA_BASE_URL =  "https://paper-api.alpaca.markets"
 
 class PythonTradingBot :
+#this method sets up the REST API
     def __init__(self):
         self.alpaca = tradeapi.REST('PK6Z8BFON57U22T0SHIE', 'Z/xVVvetdAJXoE4dJkIWT0k0mwjJc2d2Q7hY6bd6', ALPACA_BASE_URL, api_version='v2')
     def run(self) :
@@ -15,9 +28,12 @@ class PythonTradingBot :
                 self.alpaca.submit_order("MSFT", 1, 'buy', 'market', 'day')
         #Take profit at %1 increase (E.g $170 take profit at $171.7)
 
-#connect to get streaming stock market data 
+        #connect to get streaming stock market data 
         self.conn = StreamConn('polygon key', 'polygon key here', 'wss://alpaca.socket.polygon.io/stocks')
-        on_minute = self.conn(r'AM$')(on_minute)
-#subscribe to Microsoft Stock
+        on_minute = conn.on(r'AM$')(on_minute)
+
+        #subscribe to Microsoft Stock
         conn.run(['AM.MSFT'])
-bd = 
+#Run the BuyDoji class
+bd = PythonTradingBot()
+bd.run()
